bb4e027f683bdc0796880c3b0c726a220dddbaa4
Jaimee22 <alumno.410564@ies-azarquiel.es>
2024-05-16 12:42:39 +0200

    A

--- ApiPersonajesAWS/Program.cs
+++ ApiPersonajesAWS/Program.cs
@@ -3,40 +3,85 @@ using ApiPersonajesAWS.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
+[12:42] Javier Roca Pestaña
+using ApiPersonajesAWS.Data;
+
+using ApiPersonajesAWS.Repositories;
+
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.OpenApi.Models;
+ 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-string connectionString = builder.Configuration.GetConnectionString("MySqlTelevision");
-builder.Services.AddTransient<RepositoryPersonajes>();
-builder.Services.AddDbContext<PersonajesContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
+
+builder.Services.AddCors(p => p.AddPolicy("corsenabled", options =>
+
 {
+
+    options.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+
+}));
+
+string connectionString = builder.Configuration.GetConnectionString("MySqlTelevision");
+
+builder.Services.AddTransient<RepositoryPersonajes>();
+
+builder.Services.AddDbContext<PersonajesContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+
+{
+
     options.SwaggerDoc("v1", new OpenApiInfo()
+
     {
+
         Title = "Api Personajes AWS",
         Version = "v1"
+
     });
+
 });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
+
 {
 
 }
+
 app.UseSwagger();
+
 app.UseSwaggerUI(options =>
+
 {
+
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "Api Personajes AWS");
+
     options.RoutePrefix = "";
+
 });
+
 app.UseHttpsRedirection();
+
+app.UseCors("corsenabled");
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+Get started with Swashbuckle and ASP.NET Core
+Learn how to add Swashbuckle to your ASP.NET Core web API project to integrate the Swagger UI.
